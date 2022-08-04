3fd32ece27c6efcec89ee58dd100ced880aa0e7a
Tales eduardo <tales.e.valadares@gmail.com>
2022-08-04 09:59:59 -0300

    Limpeza de arquivos que foram usados apenas para fins didaticos

--- src/graphql/post/typeDefs.js
+++ src/graphql/post/typeDefs.js
@@ -2,27 +2,8 @@ import { gql } from 'apollo-server-core';
 
 export const postTypeDefs = gql`
   extend type Query {
-    post(id: ID!): PostResult!,
+    post(id: ID!): Post!,
     posts(input: ApiFiltersInput): [Post!]
-  }
-
-  union PostResult = PostNotFoundError | PostTimeoutError | Post
-
-  interface PostError {
-    statusCode: Int!
-    message: String!
-  }
-
-  type PostNotFoundError implements PostError {
-    statusCode: Int!
-    message: String!
-    postId: String!
-  }
-
-  type PostTimeoutError implements PostError {
-    statusCode: Int!
-    message: String!
-    timeout: Int!
   }
 
   type Post {
@@ -31,6 +12,5 @@ export const postTypeDefs = gql`
     body: String!
     indexRef: Int!
     createdAt: String!
-    unixTimestamp: String!
   }
 `;
